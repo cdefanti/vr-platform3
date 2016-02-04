bbc3e1643b3dc6b4a21cf690d068f5a910fe6de0
mrlabpc <mrlabpc@outlook.com>
2016-02-04 16:01:04 -0500

    added ObjectController

---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+namespace AssemblyCSharp
+{
+	public class ObjectController : MonoBehaviour
+	{
+		public string label;
+		public MasterStream mStream;
+		public void Start ()
+		{
+		}
+		public void Update() {
+			Vector3 position = Vector3.zero;
+			Quaternion rotation = Quaternion.identity;
+			if (mStream != null) {
+				position = mStream.getLiveObjectPosition (label);
+				rotation = mStream.getLiveObjectRotation (label);
+			}
+			
+			SetBodyData(position, rotation);
+		}
+		public virtual void SetBodyData(Vector3 pos, Quaternion rot) {
+			this.transform.localPosition = pos;
+			this.transform.localRotation = rot;
+		}
+	}
+}
